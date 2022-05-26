1380c101426359e0daf0ab848e1c7c729b022472
jeremyav220899 <jeremy232129@gmail.com>
2022-05-26 11:06:41 -0500

    gaaa

--- pl_Gurkas/Datos/DataReportes/RRHH/DataRRHH.cs
+++ pl_Gurkas/Datos/DataReportes/RRHH/DataRRHH.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace pl_Gurkas.Datos.DataReportes.RRHH
 {
@@ -61,5 +62,31 @@ namespace pl_Gurkas.Datos.DataReportes.RRHH
             dt.Columns[10].ColumnName = "Puesto";
             return dt;
         }
+        public DataTable ConsultarAsistenciaporPersona(string Cod_Trabajador, DateTime fechaInicio, DateTime FechaFin)
+        {
+                SqlCommand comando = conexion.conexionBD().CreateCommand();
+                comando.CommandType = CommandType.Text;
+                comando.CommandText = "SP_PersonalAsistencia  @Cod_empleado,@fechainicio, @fechafin";
+                comando.Parameters.AddWithValue("Cod_empleado", Cod_Trabajador);
+                comando.Parameters.AddWithValue("fechainicio", fechaInicio);
+                comando.Parameters.AddWithValue("fechafin", FechaFin);
+                comando.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter dta = new SqlDataAdapter(comando);
+                dta.Fill(dt);
+                dt.Columns[0].ColumnName = "Codigo Empleado";
+                dt.Columns[1].ColumnName = "Nombre Empleado";
+                dt.Columns[2].ColumnName = "Num Identidad";
+                dt.Columns[3].ColumnName = "Fecha Nacimineto";
+                dt.Columns[4].ColumnName = "Empresa";
+                dt.Columns[5].ColumnName = "Fecha Inicio";
+                dt.Columns[6].ColumnName = "Sede ";
+                dt.Columns[7].ColumnName = "Tipo Asistencia";
+                dt.Columns[8].ColumnName = "Fecha Asistencia";
+                dt.Columns[9].ColumnName = "Turno";
+                dt.Columns[10].ColumnName = "Puesto";
+                return dt;
+        }
+
     }
 }
