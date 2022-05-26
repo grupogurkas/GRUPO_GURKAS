e97f1c3ac56ea4df4cb6b102316928a0aeacf767
jeremyav220899 <jeremy232129@gmail.com>
2022-05-26 10:23:52 -0500

    ga

---
+++ pl_Gurkas/Datos/DataReportes/RRHH/DataRRHH.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pl_Gurkas.Datos.DataReportes.RRHH
+{
+    class DataRRHH
+    {
+        Datos.Conexiondbo conexion = new Datos.Conexiondbo();
+
+
+    }
+}
