63f9a19f9eec9f798f8f063e05a3f96cfa72e335
jeremyav220899 <jeremy232129@gmail.com>
2022-05-26 10:51:10 -0500

    migracion

--- pl_Gurkas/Datos/DataReportes/RRHH/DataRRHH.cs
+++ pl_Gurkas/Datos/DataReportes/RRHH/DataRRHH.cs
@@ -37,5 +37,29 @@ namespace pl_Gurkas.Datos.DataReportes.RRHH
             dt.Columns[10].ColumnName = "Puesto";
             return dt;
         }
+        public DataTable ConsultarGeneraldeAsistencia(DateTime fechaInicio, DateTime FechaFin)
+        {
+            SqlCommand comando = conexion.conexionBD().CreateCommand();
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "SP_RRHH_AsistenciaGeneralPersonal  @fechaInicio, @FechaFin";
+            comando.Parameters.AddWithValue("fechaInicio", fechaInicio);
+            comando.Parameters.AddWithValue("FechaFin", FechaFin);
+            comando.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter dta = new SqlDataAdapter(comando);
+            dta.Fill(dt);
+            dt.Columns[0].ColumnName = "Cod Empleado";
+            dt.Columns[1].ColumnName = "Empleado";
+            dt.Columns[2].ColumnName = "Num Identidad";
+            dt.Columns[3].ColumnName = "Fecha Nacimineto";
+            dt.Columns[4].ColumnName = "Empresa";
+            dt.Columns[5].ColumnName = "Fecha Inicio";
+            dt.Columns[6].ColumnName = "Sede ";
+            dt.Columns[7].ColumnName = "Tipo Asistencia";
+            dt.Columns[8].ColumnName = "Fecha Asistencia";
+            dt.Columns[9].ColumnName = "Turno";
+            dt.Columns[10].ColumnName = "Puesto";
+            return dt;
+        }
     }
 }
