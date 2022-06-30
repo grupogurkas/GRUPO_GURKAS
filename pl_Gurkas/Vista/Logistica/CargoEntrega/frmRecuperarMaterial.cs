38ac32be13635187b3ae438ab93d02a707ec694a
jeremy apaico villena <jeremy232129@hotmail.com>
2022-06-30 16:22:34 -0500

    FSF

--- pl_Gurkas/Vista/Logistica/CargoEntrega/frmRecuperarMaterial.cs
+++ pl_Gurkas/Vista/Logistica/CargoEntrega/frmRecuperarMaterial.cs
@@ -24,10 +24,18 @@ namespace pl_Gurkas.Vista.Logistica.CargoEntrega
         private void frmRecuperarMaterial_Load(object sender, EventArgs e)
         {
             Llenadocbo.ObtenerProducto(cboProducto);
+            Llenadocbo.ObtenerProducto(cboMterialRecuperado);
             txtstock.Enabled = false;
             txtcodigo.Enabled = false;
             txtstock.Text = "0";
             txtcodigo.Text = "S/C";
+            txtCodigoRecuperable.Enabled = false;
+            txtCodigoRecuperable.Text = "S/C";
+            txtstock_recuperable.Text = "0";
+
+            txtresumend.Enabled = false;
+            txtrecuperabler.Enabled = false;
+            txtrestanter.Enabled = false;
         }
 
         private void cboProducto_SelectedIndexChanged(object sender, EventArgs e)
@@ -40,8 +48,37 @@ namespace pl_Gurkas.Vista.Logistica.CargoEntrega
                 while (recorre.Read())
                 {
                     txtstock.Text = recorre["CANTIDA_DEVUELTA"].ToString();
+                    txtresumend.Text = recorre["CANTIDA_DEVUELTA"].ToString();
                     txtcodigo.Text = cod_producto;
                 }
+            }
+        }
+
+        private void cboMterialRecuperado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboProducto.SelectedValue.ToString() != null)
+            {
+                string cod_producto = cboProducto.SelectedValue.ToString();
+                txtCodigoRecuperable.Text = cod_producto;
+            }
+        }
+
+        private void txtstock_recuperable_TextChanged(object sender, EventArgs e)
+        {
+            txtresumend.Text = txtstock.Text;
+            txtrecuperabler.Text = txtstock_recuperable.Text;
+            try
+            {
+                int stock_disponible = Convert.ToInt32(txtresumend.Text);
+                int stock_recuperable = Convert.ToInt32(txtrecuperabler.Text);
+                //  int stock_restante = Convert.ToInt32(txtrestanter.Text);
+
+                int cantidad = stock_disponible - stock_recuperable;
+                txtrestanter.Text = cantidad.ToString();
+            }
+            catch(Exception ex)
+            {
+
             }
         }
     }
