e5e478504b93bba0cb096d9238ccb523548e3344
Estefano Capacyachi <103441218+SoporteTIGurkas@users.noreply.github.com>
2022-07-06 12:56:32 -0500

    AVANCE ESTEFANO

--- pl_Gurkas/Vista/Logistica/CargoEntrega/frmDestruccion.cs
+++ pl_Gurkas/Vista/Logistica/CargoEntrega/frmDestruccion.cs
@@ -38,6 +38,7 @@ namespace pl_Gurkas.Vista.Logistica.CargoEntrega
             txtRestante.Text = "0";
             txtRestante.Visible = true;
             txtvale.Enabled = false;
+            txtObservacion.Text = "EN BASE";
             string nombre_user = Datos.DatosUsuario._usuario;
             txtUsuarioEntrega.Text = nombre_user;
             Llenadocbo.ObtenerPersonalAdmi(cboPersonalAdm);
@@ -145,11 +146,13 @@ namespace pl_Gurkas.Vista.Logistica.CargoEntrega
             string ruc = lblruc.Text;
             string dir = lbldireccion.Text;
             string nombre_arc = lblnombrear.Text;
+            string hora = lblHora.Text;
             string ver = lblver.Text;
             string dni = lbldni.Text;
             string dnien = lbldnientr.Text;
             string cod = lblcodentre.Text;
             string num = txtvale.Text;
+
 
             Font tipoTexto = new Font("Arial", 10, FontStyle.Bold);
             Font desp = new Font("Arial", 8, FontStyle.Bold);
@@ -159,26 +162,26 @@ namespace pl_Gurkas.Vista.Logistica.CargoEntrega
 
             Pen blackPen = new Pen(Color.Black, 2);
 
-            Rectangle N1 = new Rectangle(20, 90, 770, 100);
+            Rectangle N1 = new Rectangle(20, 85, 770, 90);
             Rectangle N2 = new Rectangle(20, 90, 300, 50);
             Rectangle N3 = new Rectangle(20, 140, 300, 50);
             Rectangle N4 = new Rectangle(320, 140, 470, 50);
-            Rectangle observaciones = new Rectangle(30, 700, 400, 200);
+            Rectangle observaciones = new Rectangle(20, 650, 380, 220);
             Rectangle N9 = new Rectangle(20, 20, 230, 60);
             Rectangle N10 = new Rectangle(250, 20, 320, 60);
             Rectangle N11 = new Rectangle(570, 20, 220, 60);
             Rectangle N12 = new Rectangle(570, 20, 220, 30);
-            Rectangle ITEM_ = new Rectangle(20, 210, 50, 450);
-            Rectangle CODIGO_ = new Rectangle(70, 210, 60, 450);
-            Rectangle PRODUCTO_ = new Rectangle(130, 210, 400, 450);
-            Rectangle OBSERVACION_ = new Rectangle(530, 210, 100, 450);
-            Rectangle CONDICION_ = new Rectangle(630, 210, 100, 450);
-            Rectangle CANT_ = new Rectangle(730, 210, 60, 450);
-            Rectangle IMAGE_ = new Rectangle(580, 925, 210, 100);
+            Rectangle ITEM_ = new Rectangle(20, 180, 50, 480);
+            Rectangle CODIGO_ = new Rectangle(70, 180, 60, 480);
+            Rectangle PRODUCTO_ = new Rectangle(130, 180, 400, 480);
+            Rectangle OBSERVACION_ = new Rectangle(530, 180, 100, 480);
+            Rectangle CONDICION_ = new Rectangle(630, 180, 100, 480);
+            Rectangle CANT_ = new Rectangle(730, 180, 60, 480);
+            Rectangle IMAGE_ = new Rectangle(450, 700, 340, 340);
             e.Graphics.DrawRectangle(blackPen, N1);
-            e.Graphics.DrawRectangle(blackPen, N2);
-            e.Graphics.DrawRectangle(blackPen, N3);
-            e.Graphics.DrawRectangle(blackPen, N4);
+            //e.Graphics.DrawRectangle(blackPen, N2);
+            //e.Graphics.DrawRectangle(blackPen, N3);
+            //e.Graphics.DrawRectangle(blackPen, N4);
             e.Graphics.DrawRectangle(blackPen, observaciones);
             e.Graphics.DrawRectangle(blackPen, N9);
             e.Graphics.DrawRectangle(blackPen, N10);
@@ -190,10 +193,8 @@ namespace pl_Gurkas.Vista.Logistica.CargoEntrega
             e.Graphics.DrawRectangle(blackPen, OBSERVACION_);
             e.Graphics.DrawRectangle(blackPen, CONDICION_);
             e.Graphics.DrawRectangle(blackPen, CANT_);
-            //e.Graphics.DrawRectangle(blackPen, RESUMEN_);
+            e.Graphics.DrawRectangle(blackPen, IMAGE_);
             e.Graphics.DrawString("ACTA DE DESTRUCCION", tipoTexto, Brushes.Black, 310, 25);
-            //e.Graphics.DrawString(empreas, nombres, Brushes.Black, 290, 45);
-            //e.Graphics.DrawString("  RUC " + ruc, nombres, Brushes.Black, 420, 45);
             e.Graphics.DrawString(nombre_arc, datos, Brushes.Black, 580, 25);
             e.Graphics.DrawString(ver, datos, Brushes.Black, 580, 35);
             e.Graphics.DrawString(num, tipoTexto, Brushes.Black, 580, 55);
@@ -207,46 +208,48 @@ namespace pl_Gurkas.Vista.Logistica.CargoEntrega
             e.Graphics.DrawString(dir, datos, Brushes.Black, new RectangleF(260, 60, 300, 30));
             e.Graphics.DrawString("Area : ", tipoTexto, Brushes.Black, 30, 100);
             e.Graphics.DrawString(AREA_ENTREGA, desp, Brushes.Black, 120, 102);
-            //e.Graphics.DrawString("Personal: ", tipoTexto, Brushes.Black, 30, 150);
-            //e.Graphics.DrawString(PERSONAL, desp, Brushes.Black, 120, 152);
-            e.Graphics.DrawString("FECHA   : ", tipoTexto, Brushes.Black, 30, 158);
-            e.Graphics.DrawString(fecha, desp, Brushes.Black, 120, 160);
+            e.Graphics.DrawString("FECHA  : ", tipoTexto, Brushes.Black, 30, 120);
+            e.Graphics.DrawString(fecha, desp, Brushes.Black, 120, 122);
+            e.Graphics.DrawString("HORA   : ", tipoTexto, Brushes.Black, 30, 145);
+            e.Graphics.DrawString(hora, desp, Brushes.Black, 120, 147);
             e.Graphics.DrawString("DIRECCION : ", tipoTexto, Brushes.Black, 330, 100);
             e.Graphics.DrawString(DIRECCION, desp, Brushes.Black, 430, 102);
-            e.Graphics.DrawString("Encargado : ", tipoTexto, Brushes.Black, 330, 150);
-            e.Graphics.DrawString(entrega, nombres, Brushes.Black, 430, 152);
-            e.Graphics.DrawString("Personal : ", tipoTexto, Brushes.Black, 330, 170);
-            e.Graphics.DrawString(PERSONAL, desp, Brushes.Black, 430, 172);
-            //e.Graphics.DrawString("TELEFONO :", tipoTexto, Brushes.Black, 610, 150);
-            //e.Graphics.DrawString(TELEFONO, desp, Brushes.Black, 700, 152);
-            //e.Graphics.DrawString("CELULAR : ", tipoTexto, Brushes.Black, 610, 170);*/
+            e.Graphics.DrawString("Encargado Destruccion : ", tipoTexto, Brushes.Black, 330, 120);
+            e.Graphics.DrawString(entrega, nombres, Brushes.Black, 500, 122);
+            e.Graphics.DrawString("Personal Que Evidencia : ", tipoTexto, Brushes.Black, 330, 145);
+            e.Graphics.DrawString(PERSONAL, desp, Brushes.Black, 500, 147);
 
             string l1 = "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
-            e.Graphics.DrawString(l1, new System.Drawing.Font("Book Antiqua", 9, FontStyle.Bold), Brushes.Black, 20, 220);//360
+            e.Graphics.DrawString(l1, new System.Drawing.Font("Book Antiqua", 9, FontStyle.Bold), Brushes.Black, 20, 195);//360
 
             string g1 = "ITEM";
-            e.Graphics.DrawString(g1, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 30, 215);
+            e.Graphics.DrawString(g1, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 30, 190);
 
             string g2 = "CODIGO";
-            e.Graphics.DrawString(g2, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 72, 215);
+            e.Graphics.DrawString(g2, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 72, 190);
 
             string g3 = "DESCRIPCION";
-            e.Graphics.DrawString(g3, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 200, 215);
+            e.Graphics.DrawString(g3, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 200, 190);
 
             string g6 = "OBSERVACION";
-            e.Graphics.DrawString(g6, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 538, 215);
+            e.Graphics.DrawString(g6, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 538, 190);
 
             string g4 = "CONDICION";
-            e.Graphics.DrawString(g4, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 645, 215);
+            e.Graphics.DrawString(g4, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 645, 190);
 
             string g5 = "CANT.";
-            e.Graphics.DrawString(g5, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 735, 215);
+            e.Graphics.DrawString(g5, new System.Drawing.Font("Book Antiqua", 8, FontStyle.Bold), Brushes.Black, 735, 190);
+            //int y = 20;
+            //e.Graphics.DrawString("Informacion : ", tipoTexto, Brushes.Black, 40, 935);
+            e.Graphics.DrawImage(pictureBox2.Image, new Rectangle(450, 700, 340, 340));
+            e.Graphics.DrawString("Informacion : ", tipoTexto, Brushes.Black, 40, 710);
+            e.Graphics.DrawString(informacion_adicional, tipoTexto, Brushes.Black, new RectangleF(40, 725, 500, 50));
+            e.Graphics.DrawString("_______________________", tipoTexto, Brushes.Black, 40, 1000);
+            e.Graphics.DrawString("V°B° JEFE DE LOGISTICA", tipoTexto, Brushes.Black, 40, 1020);
+            e.Graphics.DrawString("_______________________", tipoTexto, Brushes.Black, 240, 1000);
+            e.Graphics.DrawString("PERSONAL QUE EVIDENCIA", tipoTexto, Brushes.Black, 240, 1020);
 
-            e.Graphics.DrawString("Informacion : ", tipoTexto, Brushes.Black, 40, 935);
-            e.Graphics.DrawString(informacion_adicional, tipoTexto, Brushes.Black, new RectangleF(40, 955, 500, 50));
-
-
-            int height = 220;
+            int height = 190;
             for (int l = numberOfItemsPrintedSoFar; l < dgvListaProducto.Rows.Count; l++)
             {
                 numberOfItemsPerPage = numberOfItemsPerPage + 1;
